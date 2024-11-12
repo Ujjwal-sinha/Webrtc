c0e8c1a7c4dfa939ac1f24c1bf9611b72d281ca2
Ujjwal Sinha <ujjwalsinha418@gmail.com>
2024-11-13 00:59:58 +0530

    websocket server done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,26 @@ import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let senderSocket: null | WebSocket = null;
-let receiverSocket: null | WebSocket = null;
+let senderSocket: any = null;
+let receiverSocket: any= null;
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data: any) {
     const message = JSON.parse(data);
+    if(message.type = "sender"){
+        senderSocket=ws;
+    }
+    else if (message.type ="receiver"){
+        receiverSocket=ws;
+    }
+    else if ( message.type="create offer"){
+       receiverSocket.send (JSON .stringify({type:"create offer", sdp:message.sdp}))
+    }
+    else if (message.type="create answer"){
+        senderSocket.send (JSON .stringify({type:"createAnswer", sdp:message.sdp}))
+    }
     
   });
 
